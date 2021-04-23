f1ae81558081d8346e7ee94020f5200ed57ad5ab
gpreviatti <gpreviatt@gmail.com>
2021-04-23 12:07:07 -0300

    imp home page layout

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -1,26 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using Domain.Dtos.EntraceTypeDto;
+using Domain.Interfaces.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Web.Models;
+using Web.ViewModels.Home;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IEntraceService _entraceService;
+        private readonly IWalletService _walletService;
+        private readonly ICategoryService _categoryService;
+        private readonly IEnumerable<EntraceTypeResultDto> _entraceTypesResultDto;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IEntraceService entraceService, IWalletService walletService, ICategoryService categoryService)
         {
-            _logger = logger;
+            _entraceService = entraceService;
+            _walletService = walletService;
+            _categoryService = categoryService;
+
+            _entraceTypesResultDto = new List<EntraceTypeResultDto>()
+            {
+                new EntraceTypeResultDto() { Value = 1, Name = "Income"},
+                new EntraceTypeResultDto() { Value = 2, Name = "Expanse"},
+                new EntraceTypeResultDto() { Value = 3, Name = "Transferance"},
+            };
         }
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
-            return View();
+            var homeIndexViewModel = new HomeIndexViewModel();
+            homeIndexViewModel.Entraces = _entraceService.FindAllAsync().Result;
+            homeIndexViewModel.Wallets = _walletService.FindAllAsync().Result;
+            homeIndexViewModel.EntraceType = _entraceTypesResultDto;
+            homeIndexViewModel.TotalExpanse = 1000;
+            homeIndexViewModel.TotalIncome = 5000;
+
+            return View(homeIndexViewModel);
         }
 
         public IActionResult Privacy()
---
+++ src/Web/ViewModels/Home/HomeIndexViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Domain.Dtos.Entrace;
+using Domain.Dtos.EntraceTypeDto;
+using Domain.Dtos.Wallet;
+
+namespace Web.ViewModels.Home
+{
+    public class HomeIndexViewModel
+    {
+        [DataType(DataType.Currency)]
+        public double? TotalIncome { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double? TotalExpanse { get; set; }
+
+        public IEnumerable<EntraceResultDto> Entraces { get; set; }
+
+        public IEnumerable<EntraceTypeResultDto> EntraceType { get; set; }
+
+        public IEnumerable<WalletResultDto> Wallets { get; set; }
+    }
+}
