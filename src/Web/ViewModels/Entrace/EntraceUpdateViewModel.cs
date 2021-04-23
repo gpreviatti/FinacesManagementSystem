40f2d2a69408fefcfe6f00518be09ffa99fabc36
gpreviatti <gpreviatt@gmail.com>
2021-04-22 21:55:30 -0300

    add view models

---
+++ src/Web/ViewModels/Entrace/EntraceUpdateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Domain.Dtos.Category;
+using Domain.Dtos.Entrace;
+using Domain.Dtos.Wallet;
+
+namespace Web.ViewModels.Entrace
+{
+    public class EntraceUpdateViewModel
+    {
+        public EntraceResultDto Entrace { get; set; }
+
+        public IEnumerable<CategoryResultDto> Categories { get; set; }
+
+        public IEnumerable<WalletResultDto> Wallets { get; set; }
+    }
+}
