eefa8a4dd22a89a2f993bca9047ecfa2261e483d
gpreviatti <gpreviatt@gmail.com>
2021-04-23 12:07:53 -0300

    fix default page

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -51,7 +51,7 @@ namespace Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Entrace}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}");
             });
         }
     }
