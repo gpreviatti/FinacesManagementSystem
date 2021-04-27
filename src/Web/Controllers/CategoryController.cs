d065af1df0d53f3ee274c86d0b57b856a8221e90
gpreviatti <gpreviatt@gmail.com>
2021-04-27 13:33:51 -0300

    add categories

--- src/Web/Controllers/CategoryController.cs
+++ src/Web/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Interfaces.Services;
+﻿using Domain.Dtos.Category;
+using Domain.Interfaces.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
@@ -16,6 +17,48 @@ namespace Web.Controllers
         {
             var categories = _service.FindAllAsync().Result;
             return View(categories);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Create(CategoryCreateDto categoryCreateDto)
+        {
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, CategoryUpdateDto categoryUpdate)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
         }
     }
 }
