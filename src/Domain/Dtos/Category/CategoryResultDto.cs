fd0b54a72404905a4a080c496eb05620bd11a3b6
gpreviatti <gpreviatt@gmail.com>
2021-04-27 19:56:40 -0300

    imp dtos

--- src/Domain/Dtos/Category/CategoryResultDto.cs
+++ src/Domain/Dtos/Category/CategoryResultDto.cs
@@ -1,12 +1,20 @@
+using System;
 using System.Collections.Generic;
 using Domain.Dtos.Entity;
 using Domain.Dtos.Entrace;
+using Domain.Dtos.User;
 
 namespace Domain.Dtos.Category
 {
     public class CategoryResultDto : EntityResultDto
     {   
         public string Name { get; set; }
+
+        public Guid UserId { get; set; }
+        public UserResultDto User { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public CategoryResultDto CategoryMain { get; set; }
 
         public IEnumerable<EntraceResultDto> Entraces { get; set; }
     }
--- src/Domain/Dtos/Category/CategoryUpdateDto.cs
+++ src/Domain/Dtos/Category/CategoryUpdateDto.cs
@@ -1,3 +1,4 @@
+using System;
 using Domain.Dtos.Entity;
 
 namespace Domain.Dtos.Category
@@ -5,5 +6,6 @@ namespace Domain.Dtos.Category
     public class CategoryUpdateDto : EntityUpdateDto
     {
         public string Name { get; set; }
+        public Guid CategoryId { get; set; }
     }
 }
