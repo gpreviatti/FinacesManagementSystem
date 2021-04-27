6d04f1d68bbb7e3a72865afa42cec821e44bf92c
gpreviatti <gpreviatt@gmail.com>
2021-04-27 19:56:32 -0300

    add user id on base service

--- src/Service/Services/BaseService.cs
+++ src/Service/Services/BaseService.cs
@@ -1,3 +1,4 @@
+using System;
 using AutoMapper;
 using Domain.Interfaces.Services;
 
@@ -6,8 +7,11 @@ namespace Service.Services
     public abstract class BaseService : IBaseService
     {
         protected IMapper _mapper;
+        protected Guid UserId { get; set; }
+
         public BaseService()
         {
+            UserId = Guid.Parse("430E0144-289F-4A95-8F14-BACFABB3FE8A");
         }
     }
 }
