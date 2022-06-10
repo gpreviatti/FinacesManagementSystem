d070f2ca029c0f892146e5fb3283ad50d4162245
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2022-06-10 20:26:37 -0300

    style: fix css

--- src/Web/wwwroot/js/Category.js
+++ src/Web/wwwroot/js/Category.js
@@ -7,7 +7,7 @@ CreateChart = (canvasId, labels, data) => {
         data: {
             labels,
             datasets: [{
-                backgroundColor: ["#375a7f"],
+                backgroundColor: ["#000"],
                 data
             }]
         }
