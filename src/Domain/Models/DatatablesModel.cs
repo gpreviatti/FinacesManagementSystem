cd8c663e7e7b001d6c8ab15e1f705e4c708627da
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2022-06-10 09:31:59 -0300

    fix: datatablesModel bug

--- src/Domain/Models/DatatablesModel.cs
+++ src/Domain/Models/DatatablesModel.cs
@@ -11,14 +11,14 @@ namespace Domain.Models
         public string SortColumnDirection { get; set; }
         public string SearchValue { get; set; }
 
-        public int pageSize;
+        private int pageSize;
         public int PageSize
         {
             get { return Length != null ? int.Parse(Length) : 0; }
             set { pageSize = value; }
         }
 
-        public int skip;
+        private int skip;
         public int Skip
         {
             get { return Start != null ? int.Parse(Start) : 0; }
