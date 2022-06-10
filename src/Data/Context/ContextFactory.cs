51aeef2d9e5cede1ef2554b3b0f026b7c9048b75
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2022-06-10 15:56:44 -0300

    feat: rollback contextFactory

---
+++ src/Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Context;
+
+public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+{
+    public MyContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<MyContext>();
+
+        builder
+            .UseNpgsql("Host=localhost;Port=5432;Database=FinancesManagementSystem;User ID=postgres;Password=admin");
+
+        return new MyContext(builder.Options);
+    }
+}
