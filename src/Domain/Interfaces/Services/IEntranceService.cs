59895efa48c2809f410165d01fdf02d5643780f1
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2022-06-13 08:50:52 -0300

    fix(30): fix codesmells

--- src/Domain/Interfaces/Services/IEntranceService.cs
+++ src/Domain/Interfaces/Services/IEntranceService.cs
@@ -3,30 +3,28 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Domain.Dtos.Entrance;
 using Domain.Dtos.EntranceTypeDto;
-using Domain.Models;
 using Domain.ViewModels;
 
-namespace Domain.Interfaces.Services
+namespace Domain.Interfaces.Services;
+
+public interface IEntranceService : IBaseService
 {
-    public interface IEntranceService : IBaseService
-    {
-        Task<EntranceResultDto> FindByIdAsync(Guid id);
-        Task<EntranceUpdateDto> FindByIdUpdateAsync(Guid id);
+    Task<EntranceResultDto> FindByIdAsync(Guid id);
+    Task<EntranceUpdateDto> FindByIdUpdateAsync(Guid id);
 
-        Task<IEnumerable<EntranceResultDto>> FindAllWithCategory(
-            string currentSort,
-            string searchString,
-            Guid userId
-        );
+    Task<IEnumerable<EntranceResultDto>> FindAllWithCategory(
+        string currentSort,
+        string searchString,
+        Guid userId
+    );
 
-        Task<IEnumerable<EntranceResultDto>> FindAsyncLastFiveEntrancesWithCategories(Guid userId);
-        List<EntranceTypeResultDto> FindEntranceTypes();
+    Task<IEnumerable<EntranceResultDto>> FindAsyncLastFiveEntrancesWithCategories(Guid userId);
+    List<EntranceTypeResultDto> FindEntranceTypes();
 
-        Task<EntranceCreateViewModel> SetupEntranceCreateViewModel(Guid userId);
-        Task<EntranceUpdateViewModel> SetupEntranceUpdateViewModel(Guid userId, Guid id);
+    Task<EntranceCreateViewModel> SetupEntranceCreateViewModel(Guid userId);
+    Task<EntranceUpdateViewModel> SetupEntranceUpdateViewModel(Guid userId, Guid id);
 
-        Task<EntranceResultDto> CreateAsync(EntranceCreateDto entraceCreateDto);
-        Task<EntranceResultDto> UpdateAsync(EntranceUpdateDto entraceUpdateDto);
-        Task<bool> DeleteAsync(Guid id);
-    }
+    Task<EntranceResultDto> CreateAsync(EntranceCreateDto entraceCreateDto);
+    Task<EntranceResultDto> UpdateAsync(EntranceUpdateDto entraceUpdateDto);
+    Task<bool> DeleteAsync(Guid id);
 }
